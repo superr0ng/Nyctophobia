b2eafa49851586bc730fedf658c8a87d48f5a954
SuperRong <superrong@SuperRongdeMacBook-Air.local>
2022-11-02 19:01:03 +0800

    toggle sign in scene2

--- Assets/Scripts/Signs.cs
+++ Assets/Scripts/Signs.cs
@@ -5,14 +5,23 @@ using UnityEngine;
 public class Signs : MonoBehaviour
 {
     public GameObject[] signs;
+    List<List<int>> characters = new List<List<int>>();
+    List<List<int>> toToggles = new List<List<int>>();
     // Start is called before the first frame update
     void Start(){
-        List<List<int>> characters = new List<List<int>>();
+        charactersInit();
+        toTogglesInit();
+        SignInit();
+        messSign();
+    }
+    void charactersInit(){
         characters.Add(new List<int> {0, 1, 2, 3, 6, 9, 12}); // L
         characters.Add(new List<int> {0, 1, 2, 4, 7, 10, 12, 13, 14}); // I
         characters.Add(new List<int> {0, 1, 2, 3, 5, 6, 8, 9, 12, 13, 14}); // G
         characters.Add(new List<int> {0, 2, 3, 5, 6, 7, 8, 9, 11, 12, 14}); // H
         characters.Add(new List<int> {1, 4, 7, 10, 12, 13, 14}); // T
+    }
+    void SignInit(){
         for(int i = 0; i < signs.Length; i++){
             var srs = signs[i].GetComponentsInChildren<SpriteRenderer>();
             foreach(int plate in characters[i]){
@@ -20,13 +29,15 @@ public class Signs : MonoBehaviour
                 srs[plate].color = Color.yellow;
             }
         }
-        
-        List<List<int>> toToggles = new List<List<int>>();
+    }
+    void toTogglesInit(){
         toToggles.Add(new List<int> {0, 7}); // L
         toToggles.Add(new List<int> {3, 13}); // I
         toToggles.Add(new List<int> {1, 4, 8}); // G
         toToggles.Add(new List<int> {0, 2, 5, 14}); // H
         toToggles.Add(new List<int> {2, 6, 9, 11, 12}); // T
+    }
+    void messSign(){
         for(int i = 0; i < signs.Length; i++){
             foreach(int toToggle in toToggles[i])
             {
@@ -37,7 +48,6 @@ public class Signs : MonoBehaviour
             }
         }
     }
-
     // Update is called once per frame
     void Update()
     {
