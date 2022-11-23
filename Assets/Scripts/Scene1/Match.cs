6d8070f51ac81ff7d886282cc6aa5c31223d9538
Kowei <b08902080@ntu.edu.tw>
2022-11-23 14:35:39 +0800

    Scene1: Let candle follow the player

--- Assets/Scripts/Scene1/Match.cs
+++ Assets/Scripts/Scene1/Match.cs
@@ -11,14 +11,14 @@ public class Match : MonoBehaviour, Iclick
     // public Animator candle2;
     public Animator player1;
     public GameObject candleObj;
-    public GameObject candle1Obj;
-    public GameObject candle2Obj;
+    // public GameObject candle1Obj;
+    // public GameObject candle2Obj;
     public void onClick(){
         candle.SetBool("isMatchClicked", true);
         // drawerWithMatch.SetActive(false);
-        DOVirtual.DelayedCall(2, Player1Getcandle);
-        DOVirtual.DelayedCall(3.9f, Player1Walking);
-        DOVirtual.DelayedCall(5.7f, Player1OpeningDoor);
+        DOVirtual.DelayedCall(1, Player1Getcandle);
+        // DOVirtual.DelayedCall(3.9f, Player1Walking);
+        // DOVirtual.DelayedCall(5.7f, Player1OpeningDoor);
         DOVirtual.DelayedCall(8, GotoNextScene);
         Destroy(gameObject);
     }
@@ -26,7 +26,7 @@ public class Match : MonoBehaviour, Iclick
         player1.SetBool("isPick", true);
         // candle.SetBool("isPicked", true);
     }
-    void Player1Walking(){
+    /* void Player1Walking(){
         candleObj.SetActive(false);
         candle1Obj.SetActive(true);
         // Walk.SetBool("isWalk", true);
@@ -37,7 +37,7 @@ public class Match : MonoBehaviour, Iclick
         candle2Obj.SetActive(true);
         // Walk.SetBool("isOpenDoor", true);
         // candle.SetBool("isOpenDoor", true);
-    }
+    } */ 
     void GotoNextScene(){
         SceneManager.LoadScene("Scene2");
         SceneManager.UnloadSceneAsync(SceneManager.GetActiveScene());
