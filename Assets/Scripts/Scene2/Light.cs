034d36f4eb69949150a60730f69975024eefb7c3
SuperRong <zxc7893212@gmail.com>
2022-12-12 19:54:54 +0800

    adding hints

--- Assets/Scripts/Scene2/Light.cs
+++ Assets/Scripts/Scene2/Light.cs
@@ -3,24 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-public class Light : MonoBehaviour, Iclick
+public class Light : MonoBehaviour, Iclick, Ihint
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
     public void onClick(){
         // Debug.Log("clicked");
 
         gameObject.GetComponent<SpriteRenderer>().color = new Color(1f, 1f, 1f, 1f);
         GetComponentInParent<LightController>().LightByObject(gameObject);
     }
-    
+    public void Hint(){
+        GetComponent<Animator>().SetTrigger("Hint");
+    }
 }
